f2da5c596ba4c34f484de02ee1e1a7c238e8bd9c
Leo Donati <leo.donati@unice.fr>
2026-09-24 12:55:45 +0200

    Add a tool bar

--- frontend-starter/src/app/components/app/app.ts
+++ frontend-starter/src/app/components/app/app.ts
@@ -1,10 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
